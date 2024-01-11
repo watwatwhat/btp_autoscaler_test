aca5c50e225b8e007503d22f46f22fd216ef2965
Takayuki Tanaka <takayuki.tanaka@sap.com>
2024-01-11 07:33:57 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// CPU intensive function using timestamps
+function performTimestampIntensiveCalculation() {
+  const startTime = new Date().getTime();
+  let count = 0;
+
+  // Example of a CPU intensive operation
+  for (let i = 0; i < 100000000; i++) {
+    for(let j=0; j<100;j++) {
+        count += 1;
+    }
+  }
+
+  const endTime = new Date().getTime();
+  return { result: count, duration: endTime - startTime };
+}
+
+app.get('/', (req, res) => {
+    res.send(`Autoscale test provided at /autoscale .`);
+  });
+
+app.get('/autoscale', (req, res) => {
+  const calculationResult = performTimestampIntensiveCalculation();
+  res.send(`Result: ${calculationResult.result}, Calculation Time: ${calculationResult.duration}ms`);
+});
+
+var port = process.env.PORT || 8080
+var server = app.listen(port, function () {
+    console.log(`Node.js is listening to PORT: ${server.address().port}`);
+});
